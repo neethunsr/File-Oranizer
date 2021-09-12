8874dfd8a3b0e7ba5c71501be8d4be4c85741b0e
neethunsr <neethu2001nsr@gmail.com>
2021-09-12 23:29:21 +0530

    python script for file organising

---
+++ file_organizer.py
@@ -0,0 +1,41 @@
+# Program to organize random files into folders based on the file extension
+# Date: 12 Sept 2021
+
+import os
+from pathlib import Path
+
+# Dictionary with folder names as key and file extensions as values
+foldernames = {
+    "Images" : [".png", ".jpeg", ".jpg", ".svg", ".gif"],
+    "Videos" : [".mp4", ".mpeg", ".mpg", ".mkv", ".mov"],
+    "Audio" : [".mp3", ".wav", ".wma"],
+    "PDF" : [".pdf"],
+    "Zip Files" : [".zip", ".rar", ".tar", ".iso", ".gz"],
+    "Documents" : [".xls", ".xslx", ".doc", ".docx", ".ppt", ".pptx", ".md", ".csv", ".json"],
+}
+
+# Map file formats to respective folders
+file_formatlist = {
+    file_format : folder 
+    for folder, file_formats in foldernames.items()
+    for file_format in file_formats
+}
+
+def organize():
+    for entry in os.scandir():
+        if entry.is_dir():
+            continue
+        file_path =  Path(entry)
+        # print(file_path)
+        file_format = file_path.suffix.lower()
+        if file_format in file_formatlist:
+            folder_path = Path(file_formatlist[file_format])
+            folder_path.mkdir(exist_ok=True)
+            file_path.rename(folder_path.joinpath(file_path))
+
+if __name__ == '__main__':
+    organize()
+
+
+
+
